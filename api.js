414bafd349f4d67ca310fe5e65370330f51b91fe
voidkiller1 <voidcla.mg7@gmail.com>
2024-06-11 19:35:03 -0300

    Reset voidmain 2.0

--- api.js
+++ api.js
@@ -27,6 +27,8 @@ async function fetchCryptoPrices() {
     }
 }
 
+
+//    Logica para o grafico + API key
 async function fetchHistoricalData() {
     const cryptocurrencies = ['BTC', 'LTC', 'ETH'];
     const currentDate = new Date().toISOString().split('T')[0];
@@ -53,72 +55,72 @@ async function fetchHistoricalData() {
     }
 }
 
-function renderChart(prices) {
-    const cryptocurrencies = Object.keys(prices);
+// function renderChart(prices) {
+//     const cryptocurrencies = Object.keys(prices);
 
-    const ctx = document.getElementById('myChart').getContext('2d');
-    const myChart = new Chart(ctx, {
-        type: 'line',
-        data: {
-            labels: Array.from({ length: 24 }, (_, i) => {
-                const date = new Date(Date.now() - (24 - i) * 60 * 60 * 1000);
-                return date.toLocaleString('en-US', { hour: 'numeric', hour12: false });
-            }),
-            datasets: cryptocurrencies.map(crypto => ({
-                label: `${crypto} Price`,
-                data: prices[crypto],
-                backgroundColor: 'transparent',
-                borderColor: randomColor(), // Gerar uma cor aleatória para cada criptomoeda
-                borderWidth: 2
-            }))
-        },
-        options: {
-            title: {
-                display: true,
-                text: 'Comparação de Preços das Criptomoedas',
-                fontColor: '#ffffff'
-            },
-            scales: {
-                xAxes: [{
-                    display: false // Oculta o eixo X
-                }],
-                yAxes: [{
-                    ticks: {
-                        fontColor: '#ffffff',
-                        callback: function(value, index, values) {
-                            return '$' + value;
-                        }
-                    }
-                }]
-            },
-            legend: {
-                labels: {
-                    fontColor: '#ffffff'
-                }
-            },
-            elements: {
-                point: {
-                    radius: 0
-                }
-            }
-        }
-    });
+//     const ctx = document.getElementById('myChart').getContext('2d');
+//     const myChart = new Chart(ctx, {
+//         type: 'line',
+//         data: {
+//             labels: Array.from({ length: 24 }, (_, i) => {
+//                 const date = new Date(Date.now() - (24 - i) * 60 * 60 * 1000);
+//                 return date.toLocaleString('en-US', { hour: 'numeric', hour12: false });
+//             }),
+//             datasets: cryptocurrencies.map(crypto => ({
+//                 label: `${crypto} Price`,
+//                 data: prices[crypto],
+//                 backgroundColor: 'transparent',
+//                 borderColor: randomColor(), // Gerar uma cor aleatória para cada criptomoeda
+//                 borderWidth: 2
+//             }))
+//         },
+//         options: {
+//             title: {
+//                 display: true,
+//                 text: 'Comparação de Preços das Criptomoedas',
+//                 fontColor: '#ffffff'
+//             },
+//             scales: {
+//                 xAxes: [{
+//                     display: false // Oculta o eixo X
+//                 }],
+//                 yAxes: [{
+//                     ticks: {
+//                         fontColor: '#ffffff',
+//                         callback: function(value, index, values) {
+//                             return '$' + value;
+//                         }
+//                     }
+//                 }]
+//             },
+//             legend: {
+//                 labels: {
+//                     fontColor: '#ffffff'
+//                 }
+//             },
+//             elements: {
+//                 point: {
+//                     radius: 0
+//                 }
+//             }
+//         }
+//     });
 
-    // Adiciona o valor máximo e mínimo ao gráfico
-    const legend = document.getElementById('legend');
-    cryptocurrencies.forEach(crypto => {
-        const listItem = document.createElement('li');
-        listItem.innerHTML = `
-            ${crypto}:
-            <div class="price-values">
-                <span class="max-price">${Math.max(...prices[crypto])}</span>
-                <span class="min-price">${Math.min(...prices[crypto])}</span>
-            </div>
-        `;
-        legend.appendChild(listItem);
-    });
-}
+//     // Adiciona o valor máximo e mínimo ao gráfico
+//     const legend = document.getElementById('legend');
+//     cryptocurrencies.forEach(crypto => {
+//         const listItem = document.createElement('li');
+//         listItem.innerHTML = `
+//             ${crypto}:
+//             <div class="price-values">
+//                 <span class="max-price">${Math.max(...prices[crypto])}</span>
+//                 <span class="min-price">${Math.min(...prices[crypto])}</span>
+//             </div>
+//         `;
+//         legend.appendChild(listItem);
+//     });
+// }
 
-function randomColor() {
-    return '#' + Math.floor(Math.random() * 16777215).toString(16); // Gerar uma cor hexadecimal aleatória
-}
+// function randomColor() {
+//     return '#' + Math.floor(Math.random() * 16777215).toString(16); // Gerar uma cor hexadecimal aleatória
+// }
